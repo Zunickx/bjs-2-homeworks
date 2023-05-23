6e14a2b3572721c2b55a72d39a7bc48f51ccf9c5
Zunickx <Zunickx@gmail.com>
2023-05-23 21:51:31 +0300

    Первая задача решена полностью

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,22 @@
 function getArrayParams(...arr) {
+  let max = arr[0];
+  let min = arr[0];
+  let sum = 0;
+  let avg;
 
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    } 
+    
+    if (arr[i] < min){
+      min = arr[i];
+    } 
+    
+    sum += arr[i];
+  }
+
+  avg = (sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
