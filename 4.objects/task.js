37b734d4feb202abcf28d3c51bd36d913e9d7332
Zunickx <Zunickx@gmail.com>
2023-06-08 21:41:37 +0300

    Create function setSubject 1.2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -8,8 +8,8 @@ const student1 = new Student ("Василиса", "женский", 19);
 const student2 = new Student ("Артём", "мужской", 25);
 const student3 = new Student("Дарья", "женский", 19);
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
