3c5f522ba84bcfad86fe52440067530adfcfe636
Zunickx <Zunickx@gmail.com>
2023-06-13 22:39:48 +0300

    Create class PrintEditionItem ver 1.0

---
+++ 5.classes/task.js
@@ -0,0 +1,28 @@
+class PrintEditionItem {
+    constructor (name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state *= 1.5;
+    }
+    
+    set state(newConditionState) {
+        if(newConditionState < 0) {
+            this._state = 0;
+        } else if(newConditionState > 100) {
+            this._state = 100;
+        } else {
+            this._state = newConditionState;
+        }
+    }
+
+    get state() {
+        return this._state;
+    }
+}
+
