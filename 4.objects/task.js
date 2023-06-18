b3189e0fc77bb32fa809363465af4a365cfd80d2
Zunickx <Zunickx@gmail.com>
2023-06-18 21:08:41 +0300

    Change addMarks ver 1.6

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -14,9 +14,7 @@ Student.prototype.setSubject = function(subjectName) {
 }
 
 Student.prototype.addMarks = function(...marks) {
-  if(this.marks === undefined) {
-    this.marks = [...marks];
-  } else {
+  if(this.marks != undefined){
     this.marks.push(...marks);
   }
 }
