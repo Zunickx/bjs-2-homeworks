4af8ee7d9ca4044791b5fd211d6d246f0386f849
Zunickx <Zunickx@gmail.com>
2023-06-23 22:39:23 +0300

    Create class AlarmClock and create addClock

---
+++ 7.async/task.js
@@ -0,0 +1,20 @@
+class AlarmClock{
+    constructor() {
+      this.alarmCollection = [];
+      this.intervalId = null;
+    }
+  
+    addClock (time, callBack) {
+      if(!time || !callBack) {
+        throw new Error('Отсутствуют обязательные аргументы');
+      }
+  
+      if(this.alarmCollection.some(element => element.time === time)) {
+        console.warn('Уже присутствует звонок на это же время');
+      }
+  
+      this.alarmCollection.push(
+        {callback, time, canCall: true}
+      )
+    }
+  }
